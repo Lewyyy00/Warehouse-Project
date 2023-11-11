aa0b768680ab86d597c16d3a8adcc23d2841c531
Michal Lewandowski <michalewy00@gmail.com>
2023-11-11 19:24:22 +0100

    Dodanie pierwszego drzewka decyzyjnego

---
+++ Projekt Magazyn.py
@@ -0,0 +1,6 @@
+print("Welcome, you are using warehouse manager")
+while True:
+    WhatToDo = input("Please choose the number based on your need:\n1.I want to exit \n2.I want to look at my data\nI want:")
+    if WhatToDo in ('1','2'):
+        break
+    print('Sorry, you picked the wrong number please choose again 1 or 2' )
