9453dd09e39cb614d1351b28c0643cd98af0558d
Michal Lewandowski <michalewy00@gmail.com>
2024-02-14 23:20:46 +0100

    add import_from_csv

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, Response
-from magazyn2 import items
+from flask import Flask, render_template, request, Response, redirect, url_for
+from magazyn import items, Product
 from forms import AddNewProductForm
-from magazyn2 import Product
 import csv
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sekretnykod'
 
@@ -24,4 +24,22 @@ def export_to_csv():
         for item_key in items:
             item = vars(items[item_key])    
             writer.writerow(item)
-    return Response("Dane zostały wyeksportowane do pliku CSV.", status=200, mimetype='text/plain')
+    return redirect(url_for("index"))
+
+@app.route("/import_from_csv")
+def import_from_csv():
+    items.clear()
+
+    with open('magazyn.csv', newline='') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        for i, row in enumerate(csv_reader):
+            product_name = row["name"]
+            product = Product(product_name, int(row["quantity"]), row["unit"], float(row["unit_price"]))
+            items[f'product_{i+1}'] = product
+
+    return redirect(url_for("index"))
+
+
+@app.route('/sell/<product_name>', methods=["GET", "POST"])
+def sell_product():
+    pass
