f77e01bfb1a3107a21a52bf64768efba14e9a829
Michal Lewandowski <michalewy00@gmail.com>
2024-12-08 15:44:52 +0100

    added new data model

--- models.py
+++ models.py
@@ -1,30 +1,45 @@
 from app import *
 
+
 class User(UserMixin, db.Model):
     __tablename__ = "users"
     
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), index=True, unique=True)
-    email = db.Column(db.String(200), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    data = db.relationship('Data', backref='author', lazy=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    products = db.relationship('UserProduct', back_populates='user', lazy=True)
+ 
+class UserProduct(db.Model):
 
-
-    def __str__(self):
-        return f"<User {self.username}>"
-    
-class Data(db.Model):
-    __tablename__ = "data"
+    __tablename__ = "user_products"
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    name = db.Column(db.String(200), index=True, unique=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
-    unit = db.Column(db.String, nullable=False)
-    unit_price = db.Column(db.Float, nullable=False)
+    user = db.relationship('User', back_populates='products')
+    product = db.relationship('Product', back_populates='user_products')
 
-    def __repr__(self):
-        return f"<Post {self.title}>"
+class Product(db.Model):
+
+    __tablename__ = "products"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=True)
+    unit = db.Column(db.String(50), nullable=False)
+    unit_price = db.Column(db.Float, nullable=False)
+    user_products = db.relationship('UserProduct', back_populates='product', lazy=True)
+
+class Category(db.Model):
+
+    __tablename__ = "categories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    products = db.relationship('Product', backref='category', lazy=True)
 
 def add_new_user(user, email, passoword):
     
