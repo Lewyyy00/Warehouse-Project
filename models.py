c89c4221e0b09a0816f3ae10015ad0a9a08bc0e4
Michal Lewandowski <michalewy00@gmail.com>
2024-12-11 19:06:39 +0100

    added universal methof of adding records of data

--- models.py
+++ models.py
@@ -41,66 +41,129 @@ class Category(db.Model):
     products = db.relationship('Product', backref='category', lazy=True)
 
 
-def add_new_user(user, email, passoword):
-    
-    new_user = User(
+class DatabaseOperations:
+
+
+    def add_new_user(user, email, passoword):
         
-        username=f"{user}",
-        email=f"{email}",
-        password_hash=f"{passoword}"
+        new_user = User(
+            
+            username = user,
+            email = email,
+            password_hash = passoword
 
-        )
-    
-    db.session.add(new_user)
-
-    try:
-        db.session.commit()
-        print(f"USER: {new_user.username} was added!")
-
-    except Exception as e:
-        db.session.rollback()
-        print(f"ERROR: {e}")
-
-    finally:
-        db.session.close()  
-
-def remove_user(user_id):
-
-    user = User.query.get(user_id)
-
-    try:
-        db.session.delete(user)
-        db.session.commit()
-        print(f"USER: {user_id} was deleted!")
-
-    except Exception as e:
-        db.session.rollback()
-        print(f"ERROR: {e}")
-
-    finally:
-        db.session.close()  
-
-
-def add_new_product(name, description, unit, unit_price):
-
-    new_product = Product(
+            )
         
-        name=f"{name}",
-        description=f"{description}",
-        unit=f"{unit}",
-        unit_price=f"{unit_price}"
+        db.session.add(new_user)
 
-        )
+        try:
+            db.session.commit()
+            print(f"USER: {new_user.username} was added!")
+
+        except Exception as e:
+            db.session.rollback()
+            print(f"ERROR: {e}")
+
+        finally:
+            db.session.close()  
+
+    def remove_user(user_id):
+
+        user = User.query.get(user_id)
+
+        try:
+            db.session.delete(user)
+            db.session.commit()
+            print(f"USER: {user_id} was deleted!")
+
+        except Exception as e:
+            db.session.rollback()
+            print(f"ERROR: {e}")
+
+        finally:
+            db.session.close()  
+            
+def add_record(model, **kwargs):
+
+    """
+    A method, that adds new record of data in table.
     
-    db.session.add(new_product)
-
+    Args:
+        model (db.Model): User, Product, Category etc.
+        **kwargs: key-value data like username="John".
+    
+    Returns:
+        record or none if error.
+    """
     try:
+        record = model(**kwargs)
+        db.session.add(record)
         db.session.commit()
-        print(f"USER: {new_product.name} was added!")
+        print(f"record was added in {model.__tablename__}: {record}")
+        return record
 
     except Exception as e:
         db.session.rollback()
         print(f"ERROR: {e}")
+        return None
 
     finally:
         db.session.close()  
+
+
+
+    """def add_new_product(name, category_id, description, unit, unit_price):
+
+        new_product = Product(
+            
+            name = name,
+            description = description,
+            category_id = category_id,
+            unit = unit,
+            unit_price = unit_price
+
+            )
+        
+        user_product = UserProduct(user_id=new_user.id, product_id=new_product.id, quantity=2)
+        db.session.add(user_product)
+        
+        db.session.add(new_product)
+
+        try:
+            db.session.commit()
+            print(f"USER: {new_product.name} was added!")
+
+        except Exception as e:
+            db.session.rollback()
+            print(f"ERROR: {e}")
+
+        finally:
+            db.session.close()  
+
+    def remove_new_product(name, description, unit, unit_price):
+
+        new_product = Product(
+        
+            name=f"{name}",
+            description=f"{description}",
+            unit=f"{unit}",
+            unit_price=f"{unit_price}"
+
+            )
+        
+    
+        db.session.add(new_product)
+
+        try:
+            db.session.commit()
+            print(f"USER: {new_product.name} was added!")
+
+        except Exception as e:
+            db.session.rollback()
+            print(f"ERROR: {e}")
+
+        finally:
+            db.session.close()  """
+
+
+
